efcbd0a26fe470faaf0c37ce7797973fc330194b
金山功樹 <bussinesskouki@gmail.com>
2024-09-15 01:53:13 +0900

    tutorial画面とコラムの実装

---
+++ daily-calender/ColumnView.swift
@@ -0,0 +1,8 @@
+//
+//  ColumnView.swift
+//  daily-calender
+//
+//  Created by 金山功樹 on 2024/09/13.
+//
+
+import Foundation
---
+++ daily-calender/TutorialView.swift
@@ -0,0 +1,8 @@
+//
+//  TutorialView.swift
+//  daily-calender
+//
+//  Created by 金山功樹 on 2024/09/13.
+//
+
+import Foundation
